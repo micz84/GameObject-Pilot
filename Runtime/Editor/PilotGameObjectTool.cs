1807c288867a61e29c9d5c6d8490e1e7f7d7f84d
micz84 <michal.leszczynski.84@gmail.com>
2023-12-22 12:58:34 +0100

    Minor fixes to improve quality of life

--- Runtime/Editor/PilotGameObjectTool.cs
+++ Runtime/Editor/PilotGameObjectTool.cs
@@ -10,6 +10,7 @@ namespace pl.micz84.gameobjectpilot.Editor
     [Icon("Packages/pl.micz84.gameobjectpilot/Runtime/Icons/camera.png")]
     public class PilotGameObjectTool : EditorToolbarToggle
     {
+        private GameObject[] _selectedGameObjects;
         public const string ID = "pilot-game-object-tool";
 
         public PilotGameObjectTool()
@@ -17,6 +18,7 @@ namespace pl.micz84.gameobjectpilot.Editor
             icon = AssetDatabase.LoadAssetAtPath<Texture2D>(
                 "Packages/pl.micz84.gameobjectpilot/Runtime/Icons/camera.png");
             tooltip = "Pilot GameObject";
+            Selection.selectionChanged += OnSelectionChangedHandler;
             GameObjectPilotOverlay.OnDisplayedChanged += OnDisplayedChangedHandler;
             RegisterCallback<ChangeEvent<bool>>(ChangeEventCallback);
         }
@@ -25,15 +27,29 @@ namespace pl.micz84.gameobjectpilot.Editor
         {
             if(isShown)
                 return;
-            
+            Selection.selectionChanged -= OnSelectionChangedHandler;
             UnregisterCallback<ChangeEvent<bool>>(ChangeEventCallback);
             SceneView.duringSceneGui -= SceneViewOnDuringSceneGui;
             GameObjectPilotOverlay.OnDisplayedChanged -= OnDisplayedChangedHandler;
         }
 
+        private void OnSelectionChangedHandler()
+        { // if tool is selected and no game object is selected take first selection
+            if (value && (_selectedGameObjects == null || _selectedGameObjects.Length == 0))
+                _selectedGameObjects = Selection.gameObjects;
+
+        }
+
         private void ChangeEventCallback(ChangeEvent<bool> evt)
         {
-            if (evt.newValue)
+            var state = evt.newValue;
+            if (state)
+                _selectedGameObjects = Selection.gameObjects;
+            else
+                _selectedGameObjects = null;
+
+            
+            if (state)
                 SceneView.duringSceneGui += SceneViewOnDuringSceneGui;
             else
                 SceneView.duringSceneGui -= SceneViewOnDuringSceneGui;
@@ -44,11 +60,10 @@ namespace pl.micz84.gameobjectpilot.Editor
             var sceneViewCameraTransform = sceneView.camera.transform;
             var position = sceneViewCameraTransform.position;
             var rotation = sceneViewCameraTransform.rotation;
-            var selectedGameObjects = Selection.gameObjects;
 
-            for (var i = 0; i < selectedGameObjects.Length; i++)
+            for (var i = 0; i < _selectedGameObjects.Length; i++)
             {
-                var t = selectedGameObjects[i].transform;
+                var t = _selectedGameObjects[i].transform;
                 t.position = position;
                 t.rotation = rotation;
             }
